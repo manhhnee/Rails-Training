77d48a0e70c6944826839daa127ff40515d9514c
Manh <manhddt2508@gmail.com>
2024-02-26 17:06:48 +0700

    Complete chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "static_pages#home"
   get "static_pages/home"
   get "static_pages/help"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "signup", to: "users#new"
+  post "signup", to: "users#create"
+  resources :users, only: :show
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_23_093746) do
+ActiveRecord::Schema.define(version: 2024_02_26_095922) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2024_02_23_093746) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.date "birthday"
+    t.string "gender"
   end
 
 end
