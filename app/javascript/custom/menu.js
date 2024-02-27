aaf531abc5c5a8329f712f3928b7ad86f7bf30d3
Manh <manhddt2508@gmail.com>
2024-02-27 16:53:19 +0700

    fix

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,8 +1,8 @@
 document.addEventListener("turbo:load", function () {
-  let account = document.querySelector("#account");
+  const account = document.querySelector("#account");
   account.addEventListener("click", function (event) {
     event.preventDefault();
-    let menu = document.querySelector("#dropdown-menu");
+    const menu = document.querySelector("#dropdown-menu");
     menu.classList.toggle("active");
   });
 });
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
            else
              BCrypt::Engine.cost
            end
-    BCrypt::Password.create string, cost: cost
+    BCrypt::Password.create string, cost
   end
 
   private
